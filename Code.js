9f9f78b88afbcf95d6bd467b9643999042a1c140
sunkenquest <sunkenquest@gmail.com>
2024-09-12 13:30:55 +0800

    fix: refactor and add gitignore

--- Code.js
+++ Code.js
@@ -1,46 +1,64 @@
+function main(e) {
+  const monitoredSpreadsheetName = "automate-report-test";
+  const sheet = e.source.getActiveSheet();
+  const range = e.range;
 
-function automateDaily(e) {
-  var monitoredSpreadsheetName = 'automate-report-test';
-  var monitoredRange = 'E9:E17';
-  
-  var sheet = e.source.getActiveSheet();
-  var range = e.range;
+  const dropdownValue = sheet.getRange("D5").getValue();
 
   if (e.source.getName() === monitoredSpreadsheetName) {
-    var monitoredCells = sheet.getRange(monitoredRange);
-    
-    if (range.getRow() >= monitoredCells.getRow() && range.getRow() <= monitoredCells.getLastRow() &&
-        range.getColumn() >= monitoredCells.getColumn() && range.getColumn() <= monitoredCells.getLastColumn()) {
-      
-      var dropdownValue = sheet.getRange('D5').getValue();
-      
-      if (dropdownValue === 'Daily Report ONLY' || dropdownValue === 'Both') {
-        
-        var newValue = range.getValue();
-        var dailyReportId = '1hmWEh-H1wraA_cLSNqvPr1a85kz2ZAmwSZou0a1LMZs'; //change this
-        var dailyReport = SpreadsheetApp.openById(dailyReportId);
-        var dailySheet = dailyReport.getSheetByName('Sheet1'); //change this
-        
-        if (dailySheet) {
-          var startCellReference = sheet.getRange('D6').getValue();
-          
-          var targetRange = getMappedRange(range, monitoredCells, dailySheet, startCellReference);
-          targetRange.setValue(newValue);
-
-          targetRange.setWrap(true);
-        }
-      }
+    if (["Daily Report ONLY"].includes(dropdownValue)) {
+      automateDaily(
+        "E9:E17",
+        "1hmWEh-H1wraA_cLSNqvPr1a85kz2ZAmwSZou0a1LMZs", // change this
+        "Sheet1", // change this
+        range,
+        sheet
+      );
     }
   }
 }
 
-function getMappedRange(sourceRange, monitoredCells, targetSheet, startCellReference) {
-  var offsetRow = sourceRange.getRow() - monitoredCells.getRow(); // Calculate row offset
-  
-  var columnLetter = startCellReference.match(/[A-Z]+/)[0];
-  var startRow = parseInt(startCellReference.match(/\d+/)[0], 10); // Extract row number (e.g., 12)
+function automateDaily(
+  monitoredDescriptions,
+  dailyReportId,
+  dailySheetName,
+  range,
+  sheet
+) {
+  const monitoredCells = sheet.getRange(monitoredDescriptions);
+  if (
+    range.getRow() >= monitoredCells.getRow() &&
+    range.getRow() <= monitoredCells.getLastRow() &&
+    range.getColumn() >= monitoredCells.getColumn() &&
+    range.getColumn() <= monitoredCells.getLastColumn()
+  ) {
+    const newValue = range.getValue();
+    const dailyReport = SpreadsheetApp.openById(dailyReportId);
+    const dailySheet = dailyReport.getSheetByName(dailySheetName);
 
-  var startColumn = columnLetter.charCodeAt(0) - 'A'.charCodeAt(0) + 1;
+    if (dailySheet) {
+      const startCellReference = sheet.getRange("D6").getValue();
+      const targetRange = getMappedDaily(
+        range,
+        monitoredCells,
+        dailySheet,
+        startCellReference
+      );
+      targetRange.setValue(newValue).setWrap(true);
+    }
+  }
+}
+
+function getMappedDaily(
+  sourceRange,
+  monitoredCells,
+  targetSheet,
+  startCellReference
+) {
+  const offsetRow = sourceRange.getRow() - monitoredCells.getRow(); // Calculate row offset
+  const columnLetter = startCellReference.match(/[A-Z]+/)[0];
+  const startRow = parseInt(startCellReference.match(/\d+/)[0], 10); // Extract row number
+  const startColumn = columnLetter.charCodeAt(0) - "A".charCodeAt(0) + 1;
 
   return targetSheet.getRange(startRow + offsetRow, startColumn);
 }
